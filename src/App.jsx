d20f3f405c57e32919672f372593d90b29939da9
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-03 13:31:23 -0500

    added path for projectspages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./HomePage";
+import ProjectsExpanded from "./ProjectsExpanded";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/projects/:id" element={<ProjectsExpanded />} />
       </Routes>
     </Router>
   );
