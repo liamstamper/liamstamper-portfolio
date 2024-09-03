c533e0023c5cbdcb4c8272715a82334fed74b154
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-03 13:33:08 -0500

    started project data for all projects

---
+++ src/projects/ProjectsData.jsx
@@ -0,0 +1,258 @@
+import GRImage1 from "../assets/gr-stocks/gr-stocks-home.png";
+import GRImage2 from "../assets/gr-stocks/gr-stocks-login.png";
+import GRImage3 from "../assets/gr-stocks/gr-stocks-side.png";
+
+export const projectsData = [
+  {
+    id: "gr-stocks",
+    title: "Gr Stocks",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "video-to-ascii",
+    title: "Video to Ascii",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "doom-in-ascii",
+    title: "Doom in Ascii",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "nasa-apod-desktop",
+    title: "Nasa Apod Desktop",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "vector-space-speeches",
+    title: "Vector Space Speech Analysis",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "open-prairie-magazine",
+    title: "Open Prarie Magazine",
+    link: "https://gr-stocks.vercel.app/",
+    repo: "https://github.com/liamstamper/gr-stocks",
+    tags: ["React", "Google Finance", "API"],
+    date: "September 2024",
+    images: [GRImage1, GRImage2, GRImage3],
+    overview:
+      "Gr Stocks is a comprehensive Google Finance clone that provides stock market data, financial news, and in-depth analytics. The application closely mirrors the core functionality of Google Finance, offering a streamlined and user-friendly interface that simplifies the process of tracking stock performance. ",
+    sections: [
+      {
+        title: "Features",
+        content:
+          "As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.",
+      },
+      {
+        title: "Technologies",
+        content:
+          "This project is built using the React library. I used an API from Alpha Vantage to get real-time stock market data. I used Tailwind CSS to style the application and icons from Google Fonts.",
+      },
+      {
+        title: "Achievements",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ],
+    bullets: [
+      {
+        quote:
+          " allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on.",
+        author: "Stewarand Design",
+      },
+      {
+        quote:
+          "a new way of sharing. It's a persistent way for everyone to see and absorb each other's work.",
+        author: "David ScEventbrite",
+      },
+    ],
+    closingStatement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
