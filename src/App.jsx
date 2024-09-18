fe3ef4b59d3a827520669ec7bceefb705c7dff42
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-18 13:06:03 -0500

    added moreprojects route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./HomePage";
 import ProjectsExpanded from "./ProjectsExpanded";
+import MoreProjects from "./Components/MoreProjects";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/projects/:id" element={<ProjectsExpanded />} />
+        <Route path="/moreprojects" element={<MoreProjects />} />
       </Routes>
     </Router>
   );
